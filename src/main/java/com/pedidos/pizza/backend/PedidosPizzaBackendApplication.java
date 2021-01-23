7a1ec3e2823e7de9dbde534708e09cfe95515325
CEIBA\alexander.lopez <alexander.lopez@ceiba.com.co>
2021-01-23 16:06:43 -0500

    Commit inicial del proyecto

---
+++ src/main/java/com/pedidos/pizza/backend/PedidosPizzaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pedidos.pizza.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosPizzaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosPizzaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedidos/pizza/backend/PedidosPizzaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedidos.pizza.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosPizzaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
